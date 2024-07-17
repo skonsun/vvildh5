fab8854579c90537f5ccebde4a6b49faecf89ca1
skonsun <skonsue@gmail.com>
2024-07-17 22:09:56 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,15 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '5a05c9f892b5796e8e43bd81';
+_mrmcp['creative_id'] = '61b95e04ee5b5b6f7945130c';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '测测你的2022人生关键词';
+_mrmcp['build_number'] = parseInt('801');
+_mrmcp['publish_time'] = 'Tue Jan 04 2022 16:23:53 GMT+0800 (CST)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+})();
